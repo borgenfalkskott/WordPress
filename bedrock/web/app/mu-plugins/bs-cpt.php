00d5f9ee4ab790d62747e568a2fe177d430b7d0a
tommyskott <tommy@borgenfalk.se>
2021-08-02 21:11:16 +0200

    Added mu-plugin to handle custom post types.

---
+++ bedrock/web/app/mu-plugins/bs-cpt.php
@@ -0,0 +1,15 @@
+<?php
+  /*
+    Plugin Name:  B&S CPT
+    Plugin URI:   https://github.com/Borgenfalk-Skott/WordPress/blob/main/bedrock/web/app/mu-plugins/bs-cpt.php
+    Description:  A custom post type plugin for WordPress.
+    Version:      1.0.0
+    Author:       Borgenfalk & Skott
+    Author URI:   https://borgenfalk.se
+    License:      MIT
+    License URI:  https://github.com/Borgenfalk-Skott/WordPress/blob/main/LICENSE
+    Text Domain:  bs-cpt
+    Domain Path:  /languages
+  */
+
+?>
